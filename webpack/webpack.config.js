abe23827b45500354002377aeecad1acdbfbff38
Kagiso Marvin Molekwa <kagisomarvin@gmail.com>
2023-06-11 17:34:53 +0200

    chore: move popup.ts to src folder.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: {
         background: path.resolve(__dirname, "..", "src", "background.ts"),
         content: path.resolve(__dirname, "..", "src", "content.ts"),
-        popup: path.resolve(__dirname, "..", "public", "popup.ts"),
+        popup: path.resolve(__dirname, "..", "src", "popup.ts"),
     },
     output: {
         path: path.resolve(__dirname, "..", "dist"),
