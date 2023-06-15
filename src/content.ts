5a88c01aefa08208e203b1e8bdcc5f022e817a10
Kagiso Marvin Molekwa <kagisomarvin@gmail.com>
2023-06-15 10:53:19 +0200

    chore: document content script.

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,8 @@
-'use-strict'
+/**
+ * Content scripts have access to the DOM and can make changes to it.
+ * The JavaScript runs in isolation, meaning it won't interfere with JavaScript running on the page, and vice versa.
+ * However, it shares the same view of the DOM as the page's scripts.
+ */
 
 import { getSubmissionEvents } from './content/submissionEvents'
 import { transformCalendar } from './content/transformCalendar'
@@ -7,6 +11,7 @@ import { PopupEvent } from './types'
 import { settings } from './utils/settings'
 
 
+// listen to events coming from the Popup window.
 chrome.runtime.onMessage.addListener(
 	async (request, _sender, _sendResponse) => {
 		const sessionKey = getSessionKey()
@@ -27,6 +32,7 @@ chrome.runtime.onMessage.addListener(
 async function main() {
 	const extensionSettings = await settings().get()
 
+	// if the radio button to enable to transform classes calendar is turned on
 	if (extensionSettings.isTransformClassesCalendar) {
 		const currentUrl = window.location.href
 		currentUrl.endsWith('mytimetable/index.php') && transformCalendar()
--- src/content/README.txt
+++ src/content/README.txt
@@ -1,1 +1,5 @@
 All code that can only run within the "content" context.
+
+* Content scripts have access to the DOM and can make changes to it.
+* The JavaScript runs in isolation, meaning it won't interfere with JavaScript running on the page, and vice versa.
+* However, it shares the same view of the DOM as the page's scripts.
